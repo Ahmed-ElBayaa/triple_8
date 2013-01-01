d2bc8f66ed4dd6946a59f9efbc2c64599f910d4f
Ahmed ElBayaa <ahmed.elbaiaa@badrit.com>
2013-01-01 16:53:00 +0200

    start point

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Triple8::Application.config.secret_token = 'ae7a08f6c366fc7f9f0998fb750033ddcbde919d5c2795326362b555585960d6183c67f41e2583512fd691155d9afb691547a2e19a2d1e3e424703cd650eaa30'
