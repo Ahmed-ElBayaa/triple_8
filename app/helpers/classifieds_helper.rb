e885804b70ac939394f64fc1010aeefdcbd45f5b
Ahmed ElBayaa <ahmed.elbaiaa@badrit.com>
2013-01-08 11:06:16 +0200

    classified's views handled the new added columns

--- app/helpers/classifieds_helper.rb
+++ app/helpers/classifieds_helper.rb
@@ -1,15 +1,7 @@
 module ClassifiedsHelper
 
-	def get_main_categories
+	def main_categories
 		Category.roots
 	end
-
-	def get_sub_categories main_category=nil
-		if main_category
-			main_category.children
-		else
-			Category.all
-		end
-	end
-
+	
 end
